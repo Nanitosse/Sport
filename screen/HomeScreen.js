f1df84d096dcc889088854dbcab06390d0dec178
Adnane <nanitosse@gmail.com>
2023-01-05 12:48:25 -0500

    reduxFavorite

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -5,38 +5,48 @@ import { FIELDS } from '../shared/field';
 // import { PARTNERS } from '../shared/partner';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from '../component/LoadingComponent';
 
 
 
 
 
-const FeaturedItem = ({item}) => {
+const FeaturedItem = ( props ) => {
+    const { item } = props;
+    if (props.isLoading) {
+        return <Loading />
+    }
+    if (props.errMess) {
+        return (
+            <View><Text>{props.errMess}</Text></View>
+        )
+    }
 
-     if (item) {
-    return (
-        <Card containerStyle={{ padding: 0, height: 200, }} >
-            <Card.Image source={{uri: baseUrl +item.image}} >
-                <View style={{ justifyContent: 'center', flex: 1 }}>
-                    <Text
-                        style={{
-                            color: 'white',
-                            textAlign: 'center',
-                            fontSize:20
-                        }}
-                    >
-                         {item.name} 
-                    </Text>
-                </View>
-            </Card.Image>
-            <Text style={{ margin:20}}>{item.desciption}</Text>
-                 
-        
-            
-           
-        </Card>
-    );
-     }
-     return <View />;
+    if (item) {
+        return (
+            <Card containerStyle={{ padding: 0, height: 200, }} >
+                <Card.Image source={{ uri: baseUrl + item.image }} >
+                    <View style={{ justifyContent: 'center', flex: 1 }}>
+                        <Text
+                            style={{
+                                color: 'white',
+                                textAlign: 'center',
+                                fontSize: 20
+                            }}
+                        >
+                            {item.name}
+                        </Text>
+                    </View>
+                </Card.Image>
+                <Text style={{ margin: 20 }}>{item.desciption}</Text>
+
+
+
+
+            </Card>
+        );
+    }
+    return <View />;
 
 
 
@@ -46,19 +56,27 @@ const HomeScreen = () => {
 
     // const [field, setField]= useState(FIELDS);
     //  const featField = field.find((item) => item.featured);
-    const fields = useSelector((state)=> state.fields);
-    const comments = useSelector((state)=>  state.comments);
-    const partners = useSelector((state)=> state.partners);
-    const featField = fields.fieldsArray.find((item)=>item.featured)
-    const featComment = comments.commentsArray.find((item)=>item.featured)
-    const featPartner = partners.partnersArray.find((item)=>item.featured);
+    const fields = useSelector((state) => state.fields);
+    const comments = useSelector((state) => state.comments);
+    const partners = useSelector((state) => state.partners);
+    const featField = fields.fieldsArray.find((item) => item.featured)
+    const featComment = comments.commentsArray.find((item) => item.featured)
+    const featPartner = partners.partnersArray.find((item) => item.featured);
 
 
     return (
         <ScrollView>
-            <FeaturedItem  item={featField}/>
-            <FeaturedItem  item={featComment}/>
-            <FeaturedItem  item={featPartner}/>
+            <FeaturedItem
+                item={featField}
+                isLoading={fields.isLoading}
+                errMess={fields.errMess}
+            />
+            <FeaturedItem item={featComment} />
+            <FeaturedItem
+                item={featPartner}
+                isLoading={partners.isLoading}
+                errMess={partners.errMess}
+            />
 
         </ScrollView>
     )
--- screen/directoryScreen.js
+++ screen/directoryScreen.js
@@ -1,28 +1,55 @@
-import { FlatList, Viwe, Text, SafeAreaView } from "react-native";
+import { FlatList, View, Text, SafeAreaView } from "react-native";
 import { Avatar, ListItem } from 'react-native-elements';
 import { useState } from "react";
 import { FIELDS } from "../shared/field";
+import Loading from "../component/LoadingComponent";
+import { useSelector } from 'react-redux';
+import { Tile } from "react-native-elements";
+import { baseUrl } from "../shared/baseUrl";
 
 
 
 
-const DirectoryScreen = ({navigation}) => {
-    const [fields , setFields]= useState(FIELDS);
+const DirectoryScreen = ({ navigation }) => {
+    const fields = useSelector((state) => state.fields)
+
+    if (fields.isLoading) {
+        return <Loading />
+    }
+    if (fields.errMess) {
+        return (
+            <View>
+                <Text>{fields.errMess}</Text>
+            </View>
+        );
+
+    }
     const renderDirectoryItem = ({ item }) => {
         return (
-            <ListItem onPress={()=>navigation.navigate('FieldInfo',{item})}r >
-                <Avatar source={item.image} />
-                <ListItem.Content>
-                    <ListItem.Title>{item.name}</ListItem.Title>
-                </ListItem.Content>
-            </ListItem>
+            // <ListItem onPress={()=>navigation.navigate('FieldInfo',{item})}r >
+            //     <Avatar source={item.image} />
+            //     <ListItem.Content>
+            //         <ListItem.Title>{item.name}</ListItem.Title>
+            //     </ListItem.Content>
+            // </ListItem>
+            <Tile
+                title={item.name}
+                caption={item.description}
+                featured
+                onPress={() =>
+                    navigation.navigate('FieldInfo', { item })
+                }
+                imageSrc={{ uri: baseUrl + item.image }}
+            />
+
+
         )
     };
 
     return (
         <FlatList
-            style={{ flex:1 }}
-            data={fields}
+            style={{ flex: 1 }}
+            data={fields.fieldsArray}
             renderItem={renderDirectoryItem}
             keyExtractor={(item) => item.id.toString()}
 
