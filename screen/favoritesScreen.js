161ca2a151a6a278d87013f220b0f09598801e6c
Adnane <nanitosse@gmail.com>
2023-01-12 14:17:10 -0500

    SwipeRow

--- screen/favoritesScreen.js
+++ screen/favoritesScreen.js
@@ -1,32 +1,49 @@
-import { useSelector } from 'react-redux';
-import { View, FlatList, Text } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { View, FlatList, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { Avatar, ListItem } from 'react-native-elements';
 import Loading from '../component/LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { SwipeRow } from 'react-native-swipe-list-view'
+import { toggleFavorite } from '../features/favorite/favoriteSlice';
 
 const FavoritesScreen = ({ navigation }) => {
     const { fieldsArray, isLoading, errMess } = useSelector((state) => state.fields);
     const favorites = useSelector((state) => state.favorites);
-    const renderFavoriteItem = ({ item  }) => {
+    const dispactch = useDispatch();
+
+
+    const renderFavoriteItem = ({ item }) => {
         return (
-            <ListItem
-                onPress={ ()=>
-                    navigation.navigate("Directory",{
-                        screen:'FieldInfo',
-                         params:{item}
+            <SwipeRow leftOpenValue={100}>
+                <View style={StyleSheet.deleteView}>
+                    <TouchableOpacity
+                        style={styles.deleteTouchable}
+                        onPress={()=>dispactch( toggleFavorite(item.id))}
+                    >
+                        <Text style = {styles.deleText}>Delete</Text>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <ListItem
+                        onPress={() =>
+                            navigation.navigate("Directory", {
+                                screen: 'FieldInfo',
+                                params: { item }
 
-                    })
-                    
+                            })
 
-                }
-            >
-                <Avatar rounded source={{uri: baseUrl + item.image}}/>
-                <ListItem.Content>
-                    <ListItem.Title>{item.name}</ListItem.Title>
-                    <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
 
-                </ListItem.Content>
-            </ListItem>
+                        }
+                    >
+                        <Avatar rounded source={{ uri: baseUrl + item.image }} />
+                        <ListItem.Content>
+                            <ListItem.Title>{item.name}</ListItem.Title>
+                            <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
+
+                        </ListItem.Content>
+                    </ListItem>
+                </View>
+            </SwipeRow >
         )
 
     }
@@ -46,11 +63,38 @@ const FavoritesScreen = ({ navigation }) => {
         <FlatList
             data={fieldsArray.filter((field) => favorites.includes(field.id))}
             renderItem={renderFavoriteItem}
-             keyExtractor={(item) => item.id.toString()}
+            keyExtractor={(item) => item.id.toString()}
         />
     )
 
 
 };
+
+
+const styles = StyleSheet.create({
+    deleteView :{
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItem: 'center', 
+        flex:1
+
+    },
+
+    deleteTouchable:{
+        backgroundColor:'red',
+        height: '100%', 
+        justifyContent: 'center'
+
+    }, 
+    deleText:{
+        color:'white',
+        fontWeight:'700',
+        textAlign: 'center',
+        fontSize: 16,
+        width: 100
+
+    }
+
+})
 
 export default FavoritesScreen
